193f5fafee1e85c6815dc5fb48a3fd335fd27d9d
SHsuperCM <shsupercm@gmail.com>
2019-09-16 09:17:05 +0300

    Added basic window

--- src/main/java/shcm/shsupercm/data/editor/SHCMDataEditor.java
+++ src/main/java/shcm/shsupercm/data/editor/SHCMDataEditor.java
@@ -1,7 +1,13 @@
 package shcm.shsupercm.data.editor;
 
-public class SHCMDataEditor {
-    public static void main(String[] args) {
+import shcm.shsupercm.data.editor.gui.frames.JFrameSHCMDataMain;
 
+import javax.swing.*;
+
+public class SHCMDataEditor {
+    public static void main(String[] args) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        JFrameSHCMDataMain mainFrame = new JFrameSHCMDataMain();
+        mainFrame.setVisible(true);
     }
 }
